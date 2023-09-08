4116bf15770a388889cebe632d09b63c44cfbfe5
AltrimH <altrim.havolli@gmail.com>
2023-09-08 16:22:33 +0200

    Added pages svervices, doctors and doctor details

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,9 +1,51 @@
-import React from "react";
+import DoctorCard from "../../components/Doctors/DoctorCard";
+import { doctors } from "../../assets/data/doctors.js";
+
+import Testimonial from '../../components/Testimonial/Testimonial'
 
 const Doctors = () => {
-    return (
-        <div>Doctors</div>
-    )
-}
+  return (
+    <>
+      <section className="bg-[#fff9ea] py-20">
+        <div className="container text-center">
+          <h2 className="heading">Find a Doctor</h2>
+          <div className="max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between">
+            <input
+              type="search"
+              className="w-full py-4 pl-4 pr-2 bg-transparent cursor-pointer focus:outline-none placeholder:text-textColor"
+              placeholder="Search Doctors"
+            />
+            <button className="btn mt-0 rounded-[0px] rounded-r-md">
+              Search
+            </button>
+          </div>
+        </div>
+      </section>
 
-export default Doctors
+      <section className="py-20">
+        <div className="container">
+          <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {doctors.map((doctor) => (
+              <DoctorCard key={doctor.id} doctor={doctor} />
+            ))}
+          </div>
+        </div>
+      </section>
+
+      <section className="py-10">
+        <div className="container">
+            <div className="xl:w-[470px] mx-auto">
+                <h2 className="text-center heading">What our patients say</h2>
+                <p className="text-center text__para">
+                    World-class care for everyone. Our health System offers unmatched, expert health care.
+                </p>
+            </div>
+
+            <Testimonial />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Doctors;
